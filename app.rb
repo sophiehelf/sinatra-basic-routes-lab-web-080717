0ab74dad54ef0715ec5582065a9254ed9908d2ac
sophiehelf <sophie.helf@gmail.com>
2017-08-28 16:06:44 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,21 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+	get '/' do 
+		"Hello World!"
+	end
+
+	get '/name' do 
+		"My name is Sophie"
+	end
+
+	get '/hometown' do 
+		"My hometown is San Francisco"
+	end
+
+	get "/favorite-song" do 
+		"My favorite song is Melody Day"
+	end
+
 end
